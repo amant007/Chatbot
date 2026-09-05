ba302d26a1db142a77744ce30053895cf043602d
Nandinip-dev <rajnandinipardeshi02@gamil.com>
2026-09-05 17:05:48 +0530

    Remove frontend and backend

--- backend/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from authentication.views import GuestSessionView, RegisterView, LoginView, ProfileView
-
-urlpatterns = [
-    path('guest/', GuestSessionView.as_view(), name='auth-guest'),
-    path('register/', RegisterView.as_view(), name='auth-register'),
-    path('login/', LoginView.as_view(), name='auth-login'),
-    path('profile/', ProfileView.as_view(), name='auth-profile'),
-]
